ad06d421391a5dff791522c947c4b64bab992885
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-28 23:02:08 +0200

    Setting up a sandbox environment and basic enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private NavMeshAgent _navMeshAgent;
+    
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("NavMeshAgent is NULL!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+}
