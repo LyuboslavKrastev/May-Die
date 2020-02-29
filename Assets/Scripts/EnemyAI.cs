da678696aa75584fc0e7a6503e701ea50ca27310
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-29 15:59:46 +0200

    Player and Enemy updates
    - Enemy can now attack the player
    - Player takes damage when attacked
    - Enemy now attempts to always face the player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [Tooltip("In meters")] [SerializeField] private float _aggroRange = 10f;
+    [SerializeField] private float _turnSpeed = 5f;
 
     private float _distanceToTarget = Mathf.Infinity;
 
@@ -39,6 +39,7 @@ public class EnemyAI : MonoBehaviour
 
     private void EngageTarget()
     {
+        FaceTarget();
         if (_distanceToTarget > _navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -51,12 +52,41 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackTarget()
     {
-        print("Attacking!");
+        Animator animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator is NULL!");
+        }
+        else
+        {
+            animator.SetBool("attack", true);
+        }
     }
 
     private void ChaseTarget()
     {
+        Animator animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator is NULL!");
+        }
+        else
+        {
+            animator.SetBool("attack", false);
+            animator.SetTrigger("move");
+        }
         _navMeshAgent.SetDestination(_target.position);
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (_target.position - transform.position).normalized;
+
+        // Z axis will be aligned with forward, X axis aligned with cross product between forward and upwards, and Y axis aligned with cross product between Z and X.
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        // Slerp - rotation which smoothly interpolates between the first quaternion (a) to the second quaternion (a
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * _turnSpeed);
     }
 
     private void OnDrawGizmosSelected()
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private int _damage = 25;
+
+    private PlayerHealth _playerHealth;
+
+    private void Start()
+    {
+        _playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if (_playerHealth == null)
+        {
+            Debug.LogError("PlayerHealth is NULL!");
+            return;
+        }
+
+        _playerHealth.TakeDamage(_damage);
+    }
+}
