f71339f6d18013df6a9806675ced023d91c58165
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-03-15 23:33:17 +0200

    Enemy Health bars, Level design
    - Enemies now have health bars
    - Updates to the level design

--- Assets/Scripts/Characters/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Characters/Enemy/EnemyHealth.cs
@@ -1,29 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] private int _hitPoints = 600;
+    private const float MAX_HEALTH = 600f;
+
+    [SerializeField] private float _currentHealth = MAX_HEALTH;
+    private Slider _healthBar;
 
     public bool IsDead { get; private set; }
 
-    public void TakeDamage(int damage)
+    void Start()
     {
-        print("taking damage");
-        _hitPoints -= damage;
-        if (_hitPoints < 1 && IsDead == false)
+        _healthBar = GetComponentInChildren<Slider>();
+        NullAlerter.AlertIfNull(_healthBar, nameof(_healthBar));
+        UpdateHealthBar(100);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        UpdateUI();
+
+        if (_currentHealth < 1 && IsDead == false)
         {
             Die();
         }
         BroadcastMessage("OnDamageTaken"); // at the moement such a method only exists within the EnemyAI class
     }
 
+    private void UpdateUI()
+    {
+        float healthPercentage = CalculateHealth();
+        UpdateHealthBar(healthPercentage);
+    }
+
+    private float CalculateHealth()
+    {
+        return _currentHealth / MAX_HEALTH;
+    }
+
+    private void UpdateHealthBar(float healthPercentage)
+    {
+        _healthBar.value = healthPercentage;
+    }
+
     private void Die()
     {
         IsDead = true;
+        _healthBar.transform.parent.gameObject.SetActive(false);
         GetComponent<Animator>().SetTrigger("die");
-        Destroy(gameObject, 10f);
+        Destroy(gameObject, 8f);
     }
 }
